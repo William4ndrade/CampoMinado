914122dd8a95fe22c4489dd7de8604e77c585c80
William Andrade <62865829+William4ndrade@users.noreply.github.com>
2021-07-26 21:59:47 -0300

    Update Main.java

--- src/com/campominado/Main.java
+++ src/com/campominado/Main.java
@@ -6,7 +6,7 @@ import com.campominado.View.Console;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	
         Tabuleiro tb = new Tabuleiro(5,5,10);
         Console c = new Console(tb);
 
